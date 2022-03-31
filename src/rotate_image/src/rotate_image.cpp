db69f6ab5cce1386abb13f77f220b19dca3e4977
Zhuoqi Wu <zhuoqiw@hotmail.com>
2022-04-01 01:44:20 +0800

    Typo

--- src/rotate_image/src/rotate_image.cpp
+++ src/rotate_image/src/rotate_image.cpp
@@ -136,8 +136,8 @@ private:
 int workers(const rclcpp::NodeOptions & options)
 {
   for (const auto& p : options.parameter_overrides()) {
-    if (p->get_name() == "workers") {
-      return p->as_int();
+    if (p.get_name() == "workers") {
+      return p.as_int();
     }
   }
   return 1;
