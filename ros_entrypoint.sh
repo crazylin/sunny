bf9cd3f95e222c7b7d98138ed09cd0a9e4c5de59
Zhuoqi Wu <zhuoqiw@hotmail.com>
2022-04-12 17:05:52 +0800

    Use multi stage Dockerfile

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -5,5 +5,5 @@ set -e
 /usr/bin/tcam-uvc-extension-loader --device=/dev/video0 -f /usr/share/theimagingsource/tiscamera/uvc-extension/usb37.json
 
 # setup ros2 environment
-source "/ws/install/setup.bash"
+source "/workspace/sunny/install/setup.bash"
 exec "$@"
