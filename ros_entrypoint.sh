af66cb3dd3b69d5e7c8977b68e2bdbcebf772e12
Zhuoqi Wu <zhuoqiw@hotmail.com>
2022-04-13 10:05:50 +0800

    Change entrypoint

--- ros_entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# load uvc extensions for USB3 33 cameras
-/usr/bin/tcam-uvc-extension-loader --device=/dev/video0 -f /usr/share/theimagingsource/tiscamera/uvc-extension/usb37.json
-
-# setup ros2 environment
-source "/workspace/sunny/install/setup.bash"
-exec "$@"
