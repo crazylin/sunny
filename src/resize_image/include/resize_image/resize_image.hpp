75c457a79872226b310ef9315fbe03d039970661
Zhuoqi Wu <zhuoqiw@hotmail.com>
2022-06-05 11:31:53 +0800

    Improve resize

--- src/resize_image/include/resize_image/resize_image.hpp
+++ src/resize_image/include/resize_image/resize_image.hpp
@@ -15,8 +15,8 @@
 #ifndef RESIZE_IMAGE__RESIZE_IMAGE_HPP_
 #define RESIZE_IMAGE__RESIZE_IMAGE_HPP_
 
-#include <memory>
-#include <utility>
+#include <deque>
+#include <vector>
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/image.hpp"
@@ -24,26 +24,147 @@
 namespace resize_image
 {
 
+using sensor_msgs::msg::Image;
+
+/**
+ * @brief Resize images implemented via opencv's resize.
+ *
+ */
 class ResizeImage : public rclcpp::Node
 {
 public:
+  /**
+   * @brief Construct a new Resize Image object.
+   *
+   * Initialize publisher.
+   * Declare parameters before usage.
+   * Create a thread for each worker.
+   * Create a thread for manager.
+   * Initialize subscription.
+   * Print success if all done.
+   * @param options Encapsulation of options for node initialization.
+   */
   explicit ResizeImage(const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
+
+  /**
+   * @brief Destroy the Resize Image object.
+   *
+   * Release subscription.
+   * Wake up all workers.
+   * Wake up the manager.
+   * Synchronize with all threads, wait for its return.
+   * Release publisher.
+   * Print success if all done.
+   * Throw no exception.
+   */
   virtual ~ResizeImage();
 
-  void Publish(sensor_msgs::msg::Image::UniquePtr & ptr)
-  {
-    _pubImage->publish(std::move(ptr));
-  }
+private:
+  /**
+   * @brief The worker works in seperate thread to process incoming date parallelly.
+   *
+   * Create a buffer.
+   * Enter infinite loop.
+   * Wait for incoming data.
+   * Wake up to get a possible data, make a promise and notify the manager.
+   * Continue to work on the data and return to sleep if no further data to process.
+   */
+  void _worker();
+
+  /**
+   * @brief The manager works in seperate thread to gather worker's results in order.
+   *
+   * Spin infinitely until rclcpp:ok() return false.
+   * Whenever a future is ready, the manager wake up, get the result from the future and publish.
+   */
+  void _manager();
+
+  /**
+   * @brief Push a image and notity workers.
+   *
+   * @param ptr Reference to a unique pointer to image to be moved.
+   */
+  void _push_back_image(Image::UniquePtr ptr);
+
+  /**
+   * @brief Promise a future so its future can be sychronized and notify the manager.
+   *
+   * @param f A future to point cloud msg.
+   */
+  void _push_back_future(std::future<Image::UniquePtr> f);
 
 private:
-  const char * _pubImageName = "~/image_resized";
-  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr _pubImage;
+  /**
+   * @brief Publisher name.
+   *
+   */
+  const char * _pub_name = "~/image_resized";
 
-  class _Impl;
-  std::unique_ptr<_Impl> _impl;
+  /**
+   * @brief Shared pointer to publisher.
+   *
+   */
+  rclcpp::Publisher<Image>::SharedPtr _pub;
 
-  const char * _subName = "~/image";
-  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr _sub;
+  /**
+   * @brief Subscription name.
+   *
+   */
+  const char * _sub_name = "~/image";
+
+  /**
+   * @brief Shared pointer to subscription.
+   *
+   */
+  rclcpp::Subscription<Image>::SharedPtr _sub;
+
+  /**
+   * @brief Number of co-workers.
+   *
+   */
+  int _workers;
+
+  /**
+   * @brief Mutex to protect image queue.
+   *
+   */
+  std::mutex _images_mut;
+
+  /**
+   * @brief Condition variable for image queue.
+   *
+   */
+  std::condition_variable _images_con;
+
+  /**
+   * @brief Double end queue for images.
+   *
+   */
+  std::deque<Image::UniquePtr> _images;
+
+  /**
+   * @brief Mutex to protect result queue.
+   *
+   */
+  std::mutex _futures_mut;
+
+  /**
+   * @brief Condition variable for result queue.
+   *
+   */
+  std::condition_variable _futures_con;
+
+  /**
+   * @brief Double end queue for results.
+   *
+   */
+  std::deque<std::future<Image::UniquePtr>> _futures;
+
+  /**
+   * @brief Threads for workers and the manager.
+   *
+   */
+  std::vector<std::thread> _threads;
 };
 
 }  // namespace resize_image
--- src/resize_image/src/resize_image.cpp
+++ src/resize_image/src/resize_image.cpp
@@ -14,145 +14,46 @@
 
 #include "resize_image/resize_image.hpp"
 
-#include <deque>
-#include <exception>
-#include <future>
-#include <map>
-#include <memory>
 #include <utility>
-#include <vector>
 
 #include "opencv2/opencv.hpp"
 
 namespace resize_image
 {
 
-using sensor_msgs::msg::Image;
-
-class ResizeImage::_Impl
+/**
+ * @brief Extract extra 'worker' parameter from ROS node options.
+ *
+ * @param options Encapsulation of options for node initialization.
+ * @return int Number of workers.
+ */
+int workers(const rclcpp::NodeOptions & options)
 {
-public:
-  explicit _Impl(ResizeImage * ptr)
-  : _node(ptr)
-  {
-    _node->declare_parameter("workers", _workers);
-    _node->get_parameter("workers", _workers);
-    _deque_size = _workers + 1;
-    for (int i = 0; i < _workers; ++i) {
-      _threads.push_back(std::thread(&_Impl::_Worker, this));
-    }
-    _threads.push_back(std::thread(&_Impl::_Manager, this));
-    RCLCPP_INFO(_node->get_logger(), "Employ %d workers successfully", _workers);
-  }
-
-  ~_Impl()
-  {
-    _images_con.notify_all();
-    _futures_con.notify_one();
-    for (auto & t : _threads) {
-      t.join();
+  for (const auto & p : options.parameter_overrides()) {
+    if (p.get_name() == "workers") {
+      return p.as_int();
     }
   }
-
-  void PushBackImage(Image::UniquePtr ptr)
-  {
-    std::unique_lock<std::mutex> lk(_images_mut);
-    _images.emplace_back(std::move(ptr));
-    auto s = static_cast<int>(_images.size());
-    if (s > _deque_size) {
-      _images.pop_front();
-    }
-    lk.unlock();
-    _images_con.notify_all();
-  }
-
-  void PushBackFuture(std::future<Image::UniquePtr> f)
-  {
-    std::unique_lock<std::mutex> lk(_futures_mut);
-    _futures.emplace_back(std::move(f));
-    lk.unlock();
-    _futures_con.notify_one();
-  }
-
-private:
-  void _Manager()
-  {
-    while (rclcpp::ok()) {
-      std::unique_lock<std::mutex> lk(_futures_mut);
-      if (_futures.empty() == false) {
-        auto f = std::move(_futures.front());
-        _futures.pop_front();
-        lk.unlock();
-        auto ptr = f.get();
-        _node->Publish(ptr);
-      } else {
-        _futures_con.wait(lk);
-      }
-    }
-  }
-
-  void _Worker()
-  {
-    Image::_data_type _buf;
-    while (rclcpp::ok()) {
-      std::unique_lock<std::mutex> lk(_images_mut);
-      if (_images.empty() == false) {
-        auto ptr = std::move(_images.front());
-        _images.pop_front();
-        std::promise<Image::UniquePtr> prom;
-        PushBackFuture(prom.get_future());
-        lk.unlock();
-        if (ptr->header.frame_id == "-1") {
-          auto img = std::make_unique<Image>();
-          img->header = ptr->header;
-          prom.set_value(std::move(ptr));
-        } else {
-          _buf.resize(ptr->width * ptr->height / 4);
-          cv::Mat dst(ptr->width / 2, ptr->height / 2, CV_8UC1, _buf.data());
-          cv::Mat img(ptr->height, ptr->width, CV_8UC1, ptr->data.data());
-          cv::resize(img, dst, cv::Size(), 0.5, 0.5);
-          std::swap(ptr->data, _buf);
-          // std::swap(ptr->width, ptr->height);
-          ptr->width /= 2;
-          ptr->height /= 2;
-          ptr->step /= 2;
-          prom.set_value(std::move(ptr));
-        }
-      } else {
-        _images_con.wait(lk);
-      }
-    }
-  }
-
-private:
-  ResizeImage * _node;
-  int _workers = 1;
-  int _deque_size;
-
-  std::mutex _images_mut;
-  std::condition_variable _images_con;
-  std::deque<Image::UniquePtr> _images;
-
-  std::mutex _futures_mut;
-  std::condition_variable _futures_con;
-  std::deque<std::future<Image::UniquePtr>> _futures;
-
-  std::vector<std::thread> _threads;
-};
+  return 1;
+}
 
 ResizeImage::ResizeImage(const rclcpp::NodeOptions & options)
 : Node("resize_image_node", options)
 {
-  _pubImage = this->create_publisher<Image>(_pubImageName, rclcpp::SensorDataQoS());
+  _pub = this->create_publisher<Image>(_pub_name, rclcpp::SensorDataQoS());
 
-  _impl = std::make_unique<_Impl>(this);
+  _workers = workers(options);
+  for (int i = 0; i < _workers; ++i) {
+    _threads.push_back(std::thread(&ResizeImage::_worker, this));
+  }
+  _threads.push_back(std::thread(&ResizeImage::_manager, this));
 
   _sub = this->create_subscription<Image>(
-    _subName,
+    _sub_name,
     rclcpp::SensorDataQoS(),
     [this](Image::UniquePtr ptr)
     {
-      _impl->PushBackImage(std::move(ptr));
+      _push_back_image(std::move(ptr));
     }
   );
 
@@ -161,11 +62,87 @@ ResizeImage::ResizeImage(const rclcpp::NodeOptions & options)
 
 ResizeImage::~ResizeImage()
 {
-  _sub.reset();
-  _impl.reset();
-  _pubImage.reset();
+  try {
+    _sub.reset();
+    _images_con.notify_all();
+    _futures_con.notify_one();
+    for (auto & t : _threads) {
+      t.join();
+    }
+    _pub.reset();
 
-  RCLCPP_INFO(this->get_logger(), "Destroyed successfully");
+    RCLCPP_INFO(this->get_logger(), "Destroyed successfully");
+  } catch (const std::exception & e) {
+    RCLCPP_ERROR(this->get_logger(), "Exception in destructor: %s", e.what());
+  } catch (...) {
+    RCLCPP_ERROR(this->get_logger(), "Exception in destructor: unknown");
+  }
+}
+
+void ResizeImage::_worker()
+{
+  Image::_data_type buf;
+  while (rclcpp::ok()) {
+    std::unique_lock<std::mutex> lk(_images_mut);
+    if (_images.empty() == false) {
+      auto ptr = std::move(_images.front());
+      _images.pop_front();
+      std::promise<Image::UniquePtr> prom;
+      _push_back_future(prom.get_future());
+      lk.unlock();
+      if (ptr->header.frame_id == "-1" || ptr->data.empty()) {
+        prom.set_value(std::move(ptr));
+      } else {
+        buf.resize(ptr->width * ptr->height / 4);
+        cv::Mat dst(ptr->width / 2, ptr->height / 2, CV_8UC1, buf.data());
+        cv::Mat img(ptr->height, ptr->width, CV_8UC1, ptr->data.data());
+        cv::resize(img, dst, cv::Size(), 0.5, 0.5);
+        std::swap(ptr->data, buf);
+        ptr->width /= 2;
+        ptr->height /= 2;
+        ptr->step /= 2;
+        prom.set_value(std::move(ptr));
+      }
+    } else {
+      _images_con.wait(lk);
+    }
+  }
+}
+
+void ResizeImage::_manager()
+{
+  while (rclcpp::ok()) {
+    std::unique_lock<std::mutex> lk(_futures_mut);
+    if (_futures.empty() == false) {
+      auto f = std::move(_futures.front());
+      _futures.pop_front();
+      lk.unlock();
+      auto ptr = f.get();
+      _pub->publish(std::move(ptr));
+    } else {
+      _futures_con.wait(lk);
+    }
+  }
+}
+
+void ResizeImage::_push_back_image(Image::UniquePtr ptr)
+{
+  std::unique_lock<std::mutex> lk(_images_mut);
+  _images.emplace_back(std::move(ptr));
+  auto s = static_cast<int>(_images.size());
+  if (s > _workers + 1) {
+    _images.pop_front();
+  }
+  lk.unlock();
+  _images_con.notify_all();
+}
+
+void ResizeImage::_push_back_future(std::future<Image::UniquePtr> f)
+{
+  std::unique_lock<std::mutex> lk(_futures_mut);
+  _futures.emplace_back(std::move(f));
+  lk.unlock();
+  _futures_con.notify_one();
 }
 
 }  // namespace resize_image
